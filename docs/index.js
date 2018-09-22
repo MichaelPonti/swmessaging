72dcd71c15e161173152a5e279d0ccb5067c3230
Michael Ponti <michael.ponti@live.ca>
2018-09-21 18:36:24 -0700

    added posting of message.

--- docs/index.js
+++ docs/index.js
@@ -1,6 +1,7 @@
 
 document.getElementById('btnmessage').addEventListener('click', function (event) {
 	console.log('test post message');
+	navigator.serviceWorker.controller.postMessage('from test button');
 });
 
 
